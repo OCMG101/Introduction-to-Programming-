70e4b10d8566c0c850badec7bb33e01baa711b2c
OCMG101 <145464133+OCMG101@users.noreply.github.com>
2023-12-16 17:40:54 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,15 +1,31 @@
 #include <stdio.h>
 
-int main( void)
-{
-   int a, b, c, d;
-   printf("Enter the first value :");
-   scanf("%d", &a );
-   printf("Enter the second value:");
-   scanf("%d", &b);
-   printf("Enter the third value :");
-   scanf("%d", &c);
-   d = a + b + c;
-   printf("%d + %d + %d = %d\n", a, b, c, d );
-   return 0;
+int main() {
+  int num1, num2, num3, greatest;
+
+  // Read three integers from the user
+  printf("Enter the first number: ");
+  scanf("%d", &num1);
+
+  printf("Enter the second number: ");
+  scanf("%d", &num2);
+
+  printf("Enter the third number: ");
+  scanf("%d", &num3);
+
+  // Compare the numbers and find the greatest
+  greatest = num1;
+  if (num2 > greatest) {
+    greatest = num2;
+  }
+  if (num3 > greatest) {
+    greatest = num3;
+  }
+
+  // Print the greatest number
+  printf("The greatest number is: %d\n", greatest);
+
+  return 0;
 }
+
+
